a9f16ad73ac8049a5141c8b842abb19e90e2c307
Veselina <vesi_9811@abv.bg>
2020-05-16 16:50:00 +0300

    Comapny Service implemented

--- src/main/java/com/company/service/CompanyServiceImpl.java
+++ src/main/java/com/company/service/CompanyServiceImpl.java
@@ -1,5 +1,13 @@
 package com.company.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.company.repository.CompanyRepository;
+
+@Service
 public class CompanyServiceImpl implements CompanyService {
 
+	@Autowired
+	private CompanyRepository companyRepository;
 }
