e330c0596544fd2131a38fedb9b024da94968b18
diamandi <diamandievd@gmail.com>
2020-05-24 14:25:41 +0300

    Shipment Serivce implementation

--- src/main/java/com/company/service/ShipmentService.java
+++ src/main/java/com/company/service/ShipmentService.java
@@ -1,5 +1,20 @@
 package com.company.service;
 
+import java.util.List;
+
+import com.company.entities.Shipment;
+
 public interface ShipmentService {
 
+	List<Shipment> findAllShipments();
+
+	Shipment findShipmentById(String id);
+
+	Shipment createShipment(Shipment shipment);
+
+	Shipment updateShipment(String id, Shipment shipment);
+
+	void deleteShipment(String id);
+
+	List<Shipment> getRegistestredShipments();
 }
--- src/main/java/com/company/service/ShipmentServiceImpl.java
+++ src/main/java/com/company/service/ShipmentServiceImpl.java
@@ -1,5 +1,58 @@
 package com.company.service;
 
-public class ShipmentServiceImpl implements ShipmentService {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.company.entities.Employee;
+import com.company.entities.Shipment;
+import com.company.repository.EmployeeRepository;
+import com.company.repository.ShipmentRepository;
+
+@Service
+public class ShipmentServiceImpl implements ShipmentService {
+	@Autowired
+	private ShipmentRepository shipmentRepository;
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public List<Shipment> findAllShipments() {
+		List<Shipment> shipments = shipmentRepository.findAll();
+		return shipments;
+	}
+
+	@Override
+	public Shipment findShipmentById(String id) {
+		return shipmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+	}
+
+	@Override
+	public Shipment createShipment(Shipment shipment) {
+		return shipmentRepository.save(shipment);
+	}
+
+	@Override
+	public Shipment updateShipment(String id, Shipment shipment) {
+		return shipmentRepository.save(shipment);
+	}
+
+	@Override
+	public void deleteShipment(String id) {
+		Shipment shipment = shipmentRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+		shipmentRepository.delete(shipment);
+	}
+
+	@Override
+	public List<Shipment> getRegistestredShipments() {
+		List<Shipment> registred = new ArrayList<>();
+		List<Employee> employees = this.employeeRepository.findAll();
+		for (Employee employee : employees) {
+			registred.addAll(employee.getShipments());
+		}
+		return registred;
+	}
 }
